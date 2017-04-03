1ac231197c8f206d810c0837fad6862b61caa908
Ekhlas Rain <baran.rain1994@gmail.com>
2017-04-03 18:46:42 +0300

    add projects and skill

---
+++ js/project.js
@@ -0,0 +1,29 @@
+// $.get('https://api.github.com/users/EkhlasRain/repos?sort=created',function(data){
+
+// }
+$(document).ready(function(){
+  loadProjects();
+  $('#btnFav').click(function(){
+    $('#favourite').fadeIn();
+  })
+  $('#currentBtn').click(loadProjects)
+});
+function loadProjects(){
+  $.ajax({
+    url: 'https://api.github.com/users/EkhlasRain/repos?sort=created&per_page=5',
+    type: 'GET',
+    success: function(json){
+      result(json);
+    }
+  })
+}
+function result(data){
+  var html= '';
+  html+= '<ul>';
+  $.each(data, function(i,item){
+    // html+= '<li><h1>' + item.name +'</h1>';
+    html+= '<li><a href="'+ item.html_url+'" target="_blank">' + item.name +'</a></li>';
+  })
+  html+= '</ul>'
+  $('#result').html(html);
+}
